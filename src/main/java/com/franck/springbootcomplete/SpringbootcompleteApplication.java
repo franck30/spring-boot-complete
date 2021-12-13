c2d0dfbf89527401b6cf58f863151e62fab62604
tchuitiocarin <tchuitiocarin@gmail.com>
2021-12-13 04:03:19 +0100

    first commit

---
+++ src/main/java/com/franck/springbootcomplete/SpringbootcompleteApplication.java
@@ -0,0 +1,13 @@
+package com.franck.springbootcomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcompleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franck/springbootcomplete/SpringbootcompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franck.springbootcomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
