8f95f00f2077b723ae4c8b9fc6c4158819747c2c
tchuitiocarin <tchuitiocarin@gmail.com>
2021-12-14 04:30:12 +0100

    created controller and service class for department

---
+++ src/main/java/com/franck/springbootcomplete/service/DepartmentService.java
@@ -0,0 +1,7 @@
+package com.franck.springbootcomplete.service;
+
+import com.franck.springbootcomplete.entity.Department;
+
+public interface DepartmentService  {
+  public Department saveDepartment(Department department);
+}
---
+++ src/main/java/com/franck/springbootcomplete/service/DepartmentServiceImpl.java
@@ -0,0 +1,14 @@
+package com.franck.springbootcomplete.service;
+
+
+import com.franck.springbootcomplete.entity.Department;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+
+  @Override
+  public Department saveDepartment(Department department) {
+    return null;
+  }
+}
