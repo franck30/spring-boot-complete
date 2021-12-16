a5ef71202a7fc113bd385e6b72642d50be088890
tchuitiocarin <tchuitiocarin@gmail.com>
2021-12-16 04:03:13 +0100

    added method to get department based on name

--- src/main/java/com/franck/springbootcomplete/controller/DepartmentController.java
+++ src/main/java/com/franck/springbootcomplete/controller/DepartmentController.java
@@ -46,4 +46,11 @@ public class DepartmentController {
     return departmentService.updateDepartment(departmentId, department);
   }
 
+
+  @GetMapping("/departments/name/{name}")
+  public Department fetchDepartmentByName(@PathVariable("name") String departmentName) {
+
+    return departmentService.fetchDepartmentByName(departmentName);
+  }
+
 }
--- src/main/java/com/franck/springbootcomplete/repository/DepartmentRepository.java
+++ src/main/java/com/franck/springbootcomplete/repository/DepartmentRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long>{
 
+
+  public Department findByDepartmentName(String departmentName);
 }
