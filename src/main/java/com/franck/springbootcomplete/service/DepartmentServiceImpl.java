c3c0bd408ca45843f8990175299864b096559026
tchuitiocarin <tchuitiocarin@gmail.com>
2021-12-16 03:32:07 +0100

    added the post web service for department

--- src/main/java/com/franck/springbootcomplete/service/DepartmentServiceImpl.java
+++ src/main/java/com/franck/springbootcomplete/service/DepartmentServiceImpl.java
@@ -2,13 +2,19 @@ package com.franck.springbootcomplete.service;
 
 
 import com.franck.springbootcomplete.entity.Department;
+import com.franck.springbootcomplete.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
 
+  @Autowired
+  private DepartmentRepository departmentRepository;
+
+
   @Override
   public Department saveDepartment(Department department) {
-    return null;
+    return departmentRepository.save(department);
   }
 }
