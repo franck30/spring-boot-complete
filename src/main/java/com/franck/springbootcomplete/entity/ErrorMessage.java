4c5e9519de6d4626533f7f5397babcf84e9362b5
tchuitiocarin <tchuitiocarin@gmail.com>
2021-12-17 04:55:01 +0100

    added exception handling in controller layer

---
+++ src/main/java/com/franck/springbootcomplete/entity/ErrorMessage.java
@@ -0,0 +1,17 @@
+package com.franck.springbootcomplete.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+
+
+  private HttpStatus status;
+  private String message;
+
+}
