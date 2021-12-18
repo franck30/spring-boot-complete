0d00d005155c03f4f0da3e83d85fd8b76c8f4043
tchuitiocarin <tchuitiocarin@gmail.com>
2021-12-18 06:27:32 +0100

    changed application properties to yaml

--- src/main/java/com/franck/springbootcomplete/controller/DepartmentController.java
+++ src/main/java/com/franck/springbootcomplete/controller/DepartmentController.java
@@ -8,6 +8,7 @@ import com.franck.springbootcomplete.service.DepartmentServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,15 @@ public class DepartmentController {
   @Autowired
   private DepartmentService departmentService;
 
+  @Value("${welcome.message}")
+  private String welcomeMessage;
+
   private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+  @GetMapping("/")
+  public String helloWorld() {
+    return welcomeMessage;
+  }
 
   @PostMapping("/departments")
   @ResponseStatus(HttpStatus.CREATED)
